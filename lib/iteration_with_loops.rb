96168254d769c889bbb9afdb2c611ef959085cb4
clean-loop-a64b <dsjong2002@yahoo.com>
2021-05-26 07:12:22 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,13 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  ans = []
+  src.each do |a|
+    a.each do |x|
+      if String === x
+        ans += [x]
+      end
+    end
+  end
+  ans.join(' ')
 end
